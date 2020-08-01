e60e9f0532a45130902be0e9499d4164c1236a67
chiaqingwu <secret8419@gmail.com>
2020-08-01 11:15:12 +0800

    feat(uialert): add the UIAlertController to apply to select

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -16,16 +16,16 @@ class RestaurantTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.cellLayoutMarginsFollowReadableWidth = true
     }
 
     // MARK: - Table view data source
-
+    var checkedList = [
+        false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false
+    ]
+    
     let restaurant = [
             "楠梓區", "左營區", "鼓山區", "三民區", "鹽埕區", "前金區", "新興區",
             "苓雅區", "前鎮區", "小港區", "旗津區", "鳳山區", "大寮區", "鳥松區",
@@ -58,6 +58,9 @@ class RestaurantTableViewController: UITableViewController {
         cell.locationLabel.text = restaurantLocation[indexPath.row]
         cell.typeLabel.text = restaurantType[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        
+        cell.accessoryType = (checkedList[indexPath.row] == true) ? .checkmark : .none
+        
         return cell
     }
     
@@ -70,60 +73,48 @@ class RestaurantTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return restaurant.count
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //MARK: - Phone alert
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let callAction = UIAlertAction(title: "Call" + "0800-009-\(indexPath.row)", style: .default, handler: {
+            (action: UIAlertAction!) -> Void in
+            
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature isn't available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+            
+        })
+        
+        let checkMarkAction = UIAlertAction(title: "Check Mark", style: .default, handler: {
+            (action: UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            
+            if cell?.accessoryType == .checkmark {
+                cell?.accessoryType = .none
+                self.checkedList[indexPath.row] = false
+            } else {
+                cell?.accessoryType = .checkmark
+                self.checkedList[indexPath.row] = true
+            }
+            
+        })
+        
+        //MARK: - Pad alert
+        if let popoverController = optionMenu.popoverPresentationController,
+            let cell = tableView.cellForRow(at: indexPath) {
+            popoverController.sourceView = cell
+            popoverController.sourceRect = cell.bounds
+        }
+        
+        optionMenu.addAction(callAction)
+        optionMenu.addAction(checkMarkAction)
+        optionMenu.addAction(cancelAction)
+        present(optionMenu, animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
