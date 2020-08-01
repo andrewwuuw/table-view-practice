72be3dd6be059f088eadabf5b35be94e1c99e30d
chiaqingwu <secret8419@gmail.com>
2020-08-01 08:08:06 +0800

    feat: add some restaurants

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by chiaqingwu on 2020/7/31.
+//  Copyright © 2020 Andrewwuuw. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    //MARK: - UI
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
